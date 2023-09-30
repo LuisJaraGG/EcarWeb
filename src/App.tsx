a28c3ee18f0859024c42a1f04957f2a5aee61f43
Luis albarran <luisalbarran21rl04091216@email.com>
2023-09-29 21:29:53 -0500

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,31 @@ import Services from "./components/Services";
 import NavBar from "./components/NavBar";
 import Plans from "./components/Plans";
 import NavMovil from "./components/NavMovil";
+import Spiner from "./components/spiner";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 
 function App() {
+  const [loading, setloading] = useState(true)
   useEffect(() => {
     AOS.init();
+    setTimeout(() => {
+      setloading(false)
+    }, 1500);
   }, []);
 
   return (
-    <>
-      <NavMovil></NavMovil>
-      <NavBar></NavBar>
-      <Hero></Hero>
-      <Services></Services>
-      <Plans></Plans>
-    </>
+    <div>
+      {loading && <Spiner />}
+      <div className={loading ? "hidden" : ""}>
+        <NavMovil></NavMovil>
+        <NavBar></NavBar>
+        <Hero></Hero>
+        <Services></Services>
+        <Plans></Plans>
+      </div>
+    </div>
   );
 }
 
